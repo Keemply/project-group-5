62b378dd601417114de028f72aaeb89375f5db07
SvitlanaTesliuk <149165687+SvitlanaTesliuk@users.noreply.github.com>
2024-10-15 19:32:41 +0300

    change night theme2

--- src/js/faq-section.js
+++ src/js/faq-section.js
@@ -14,5 +14,35 @@ document.addEventListener('DOMContentLoaded', function () {
     onClose: function (currentElement) {
       currentElement.parentElement.classList.remove('open'); 
     }
+    
   });
+  console.log(document.querySelector('.accordion'));
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const accordions = document.querySelectorAll('.accordion-item');
+  
+  if (accordions.length > 0) {
+    accordions[0].classList.add('open');
+    const firstBody = accordions[0].querySelector('.accordion-body');
+    firstBody.style.display = 'block';
+}
+
+accordions.forEach(item => {
+    const header = item.querySelector('.accordion-header');
+
+    header.addEventListener('click', () => {
+        
+        accordions.forEach(i => {
+            if (i !== item) {
+                i.classList.remove('open');
+                i.querySelector('.accordion-body').style.display = 'none';
+            }
+        });
+
+       
+        item.classList.toggle('open');
+        const body = item.querySelector('.accordion-body');
+        body.style.display = item.classList.contains('open') ? 'block' : 'none';
+    });
+});
 });
